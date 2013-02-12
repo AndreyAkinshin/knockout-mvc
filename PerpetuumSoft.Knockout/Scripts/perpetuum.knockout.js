b7def2f5e8f8b5f0cded9ce8c82045b2451b3e02
Andrey Akinshin <andrey.akinshin@perpetuumsoft.com>
2013-02-12 18:21:40 +0700

    Add NuGet package

---
+++ PerpetuumSoft.Knockout/Scripts/perpetuum.knockout.js
@@ -0,0 +1,17 @@
+﻿executeOnServer = function (model, url) {
+
+    $.ajax({
+        url: url,
+        type: 'POST',
+        data: ko.mapping.toJSON(model),
+        dataType: "json",
+        contentType: "application/json; charset=utf-8",
+        success: function (data) {
+            ko.mapping.fromJS(data, model);
+        },
+        error: function (error) {
+            alert("There was an error posting the data to the server: " + error.responseText);
+        }
+    });
+
+};
