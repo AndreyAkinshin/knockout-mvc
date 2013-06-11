98e385edb91f36efb14405e8717f2c69f58d979b
drvic10k <matus.brlit@gmail.com>
2013-06-11 08:47:35 -0700

    Added option to allow redirecting after ServerAction

--- PerpetuumSoft.Knockout/KnockoutContext.cs
+++ PerpetuumSoft.Knockout/KnockoutContext.cs
@@ -202,13 +202,13 @@ namespace PerpetuumSoft.Knockout
       }
     }
 
-    public MvcHtmlString ServerAction(string actionName, string controllerName, object routeValues = null)
+    public MvcHtmlString ServerAction(string actionName, string controllerName, object routeValues = null,bool redirect=false)
     {
       var url = Url().Action(actionName, controllerName, routeValues);
       url = url.Replace("%28", "(");
       url = url.Replace("%29", ")");
       url = url.Replace("%24", "$");
-      string exec = string.Format(@"executeOnServer({0}, '{1}')", ViewModelName, url);
+      string exec = string.Format(@"{0}({1}, '{2}')",redirect?"executeOnServerAndRedirect":"executeOnServer", ViewModelName, url);
       int startIndex = 0;
       const string parentPrefix = "$parentContext.";
       while (exec.Substring(startIndex).Contains("$index()"))
--- PerpetuumSoft.Knockout/Scripts/perpetuum.knockout.js
+++ PerpetuumSoft.Knockout/Scripts/perpetuum.knockout.js
@@ -15,3 +15,21 @@
     });
 
 };
+
+executeOnServerAndRedirect = function (model, url) {
+
+    $.ajax({
+        url: url,
+        type: 'POST',
+        data: ko.mapping.toJSON(model),
+        dataType: "json",
+        contentType: "application/json; charset=utf-8",
+        success: function (data) {
+            location.href = data.url;
+        },
+        error: function (error) {
+            alert("There was an error posting the data to the server: " + error.responseText);
+        }
+    });
+
+};
