2e7a908f225c0a93e98ebbdde7755457455ebac8
plmeister <plmeister@gmail.com>
2013-08-15 10:57:42 +0100

    Update install.ps1
    add support for VB projects on install

--- Install/install.ps1
+++ Install/install.ps1
@@ -1,8 +1,19 @@
 param($installPath, $toolsPath, $package, $project)
 
-$item = $project.ProjectItems.Item("global.asax").ProjectItems.Item("global.asax.cs")
-$namespace = $item.FileCodeModel.CodeElements | where-object {$_.Kind -eq 5}
-$class = $namespace.Children | where-object {$_.Kind -eq 1}
+$projectIsVB = $false
+try {
+    $item = $project.ProjectItems.Item("global.asax").ProjectItems.Item("global.asax.cs")
+} catch {
+    $item = $project.ProjectItems.Item("global.asax").ProjectItems.Item("global.asax.vb")
+    $projectIsVB = $true
+}
+
+if ($projectIsVB) {
+    $class = $item.FileCodeModel.CodeElements | where-object {$_.Kind -eq 1}
+} else {
+    $namespace = $item.FileCodeModel.CodeElements | where-object {$_.Kind -eq 5}
+    $class = $namespace.Children | where-object {$_.Kind -eq 1}
+}
 
 $method = $class.Children | where-object {$_.Name -eq "Application_Start"}
 if (!$method)
@@ -14,4 +25,9 @@ $edit = $method.StartPoint.CreateEditPoint();
 $edit.LineDown()
 $edit.CharRight(1)
 $edit.Insert([Environment]::NewLine)
-$edit.Insert("      ModelBinders.Binders.DefaultBinder = new PerpetuumSoft.Knockout.KnockoutModelBinder();")
+if ($projectIsVB) {
+    $edit.Insert("      ModelBinders.Binders.DefaultBinder = new PerpetuumSoft.Knockout.KnockoutModelBinder()")
+    $edit.Insert([Environment]::Newline)
+} else {
+    $edit.Insert("      ModelBinders.Binders.DefaultBinder = new PerpetuumSoft.Knockout.KnockoutModelBinder();")
+}
