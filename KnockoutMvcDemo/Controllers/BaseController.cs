5be58f99e17910f54582a3981a94b8fc99e015d4
Andrey Akinshin <andrey.akinshin@gmail.com>
2014-08-08 14:28:20 +0700

    Minor update of BaseController.cs

--- KnockoutMvcDemo/Controllers/BaseController.cs
+++ KnockoutMvcDemo/Controllers/BaseController.cs
@@ -26,10 +26,10 @@ namespace KnockoutMvcDemo.Controllers
         protected void InitializeViewBag(string caption = "")
         {
             var modelName = GetType().Name.Substring(0, GetType().Name.Length - "Controller".Length);
-            ViewBag.Model = GetModel(modelName);
-            ViewBag.Controller = GetController(modelName);
-            ViewBag.Razor = GetRazor(modelName);
-            ViewBag.Description = new MvcHtmlString(GetDescription(modelName));
+            ViewBag.Model = FormatModelCode(modelName);
+            ViewBag.Controller = FormatControllerCode(modelName);
+            ViewBag.Razor = FormatRazorCode(modelName);
+            ViewBag.Description = new MvcHtmlString(FormatDescription(modelName));
             if (!string.IsNullOrWhiteSpace(caption))
             {
                 ViewBag.Title = caption;
@@ -37,17 +37,17 @@ namespace KnockoutMvcDemo.Controllers
             }
         }
 
-        private string GetModel(string modelName)
+        private string FormatModelCode(string modelName)
         {
-            return GetCode(LoadFile(string.Format("\\Models\\{0}Model.cs", modelName)));
+            return FormatSourceCode(LoadFile(string.Format("\\Models\\{0}Model.cs", modelName)));
         }
 
-        private string GetController(string modelName)
+        private string FormatControllerCode(string modelName)
         {
-            return GetCode(LoadFile(string.Format("\\Controllers\\{0}Controller.cs", modelName)));
+            return FormatSourceCode(LoadFile(string.Format("\\Controllers\\{0}Controller.cs", modelName)));
         }
 
-        private string GetCode(string fileName)
+        private string FormatSourceCode(string fileName)
         {
             var split = fileName.Split(new[] { '\n' }).ToList();
             while (split.Count > 0)
@@ -62,23 +62,23 @@ namespace KnockoutMvcDemo.Controllers
                 split.RemoveAt(split.Count - 1);
             if (split.Count > 0 && split.Last().Trim() == "}")
                 split.RemoveAt(split.Count - 1);
-            var sb = new StringBuilder();
+            var builder = new StringBuilder();
             foreach (var line in split)
             {
-                if (sb.Length > 0)
-                    sb.Append('\n');
-                sb.Append(line.StartsWith("  ") ? line.Substring(2) : line);
+                if (builder.Length > 0)
+                    builder.Append('\n');
+                builder.Append(line.StartsWith("    ") ? line.Substring(4) : line);
             }
-            return sb.ToString();
+            return builder.ToString();
         }
 
-        private string GetRazor(string modelName)
+        private string FormatRazorCode(string modelName)
         {
             var razor = LoadFile(string.Format("\\Views\\{0}\\Index.cshtml", modelName));
             return razor;
         }
 
-        private string GetDescription(string modelName)
+        private string FormatDescription(string modelName)
         {
             string description = LoadFile(string.Format("\\Descriptions\\{0}.html", modelName));
             if (string.IsNullOrWhiteSpace(description))
