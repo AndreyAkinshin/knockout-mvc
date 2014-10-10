7bc6159b69445631dc7ec06d61185c0f01346278
Rob Hill <rhill@aardvarkapps.com>
2014-10-09 19:00:29 -0700

    Fix stack overflow when trying to convert an object of a given type such that the type has static properties of the same type

---
+++ PerpetuumSoft.Knockout.Tests/KnockoutUtilitiesTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PerpetuumSoft.Knockout.Tests
+{
+    [TestClass]
+    public class KnockoutUtilitiesTest
+    {
+        [TestMethod]
+        public void ConvertDataTest01()
+        {
+            TestModel model = new TestModel();
+            bool doesNotCauseStackOverflow = false;
+
+            Knockout.KnockoutUtilities.ConvertData(model);
+            
+            doesNotCauseStackOverflow = true;
+            Assert.IsTrue(doesNotCauseStackOverflow);
+        }
+    }
+}
